5a0bab47c626a3a503a836bf499cad11c8071576
sofialovgren <Sofia.Lovgren.4526@student.uu.se>
2019-12-06 14:54:09 +0100

    kitchen summa funkar

--- src/views/Kitchen.vue
+++ src/views/Kitchen.vue
@@ -1,5 +1,15 @@
 <template>
 <div id="orders">
+
+  <div id="summaryOrder">
+     <h1> {{uiLabels.ordersSummary}}</h1>
+     <div v-for="countIng in countAllIngredients"
+           v-if="countIng.count>0"
+           :key="countAllIngredients.indexOf(countIng)">
+         {{countIng.name}}: {{countIng.count}}
+   </div>
+  </div>
+
 <div id="ordersQueue">
   <h1>{{ uiLabels.ordersInQueue }}</h1>
     <OrderItemToPrepare
@@ -15,13 +25,6 @@
     </OrderItemToPrepare>
   </div>
 
-  <div id="summaryOrder">
-     <h1> {{uiLabels.ordersSummary}}</h1>
-
-
-
-
-  </div>
 
   <div id="finishedOrder">
     <h1>{{ uiLabels.ordersFinished }}</h1>
@@ -61,14 +64,46 @@ export default {
       price: 0
     }
   },
+
+  computed: {
+      countBeef100: function() {
+        return this.countNumberOfIngredients(2)
+      },
+      countAllIngredients: function() {
+        let ingredientTuples = []
+        for (let i = 0; i < this.ingredients.length; i += 1) {
+          ingredientTuples[i] = {};
+          ingredientTuples[i].name = this.ingredients[i]['ingredient_' + this.lang];
+          ingredientTuples[i].count = this.countNumberOfIngredients(this.ingredients[i].ingredient_id);
+        }
+        return ingredientTuples;
+      }
+    },
+
   methods: {
 
     markDone: function (orderid) {
       this.$store.state.socket.emit("orderDone", orderid);
-    }
-  }
+    },
+
+
+  countNumberOfIngredients: function (id) {
+     let counter = 0;
+     for (let order in this.orders) {
+       for (let i = 0; i < this.orders[order].ingredients.length; i += 1) {
+         if (this.orders[order].ingredients[i].ingredient_id === id) {
+           counter +=1;
+         }
+       }
+     }
+     return counter;
+   }
+ }
+
 }
 </script>
+
+
 <style scoped>
 
 
