b2e9f1885f166b91d9d44d4d727d5008e51dbba4
eriklundman <erik.lundman@bellkom.se>
2019-12-10 17:46:24 +0100

    small button changes

--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -233,6 +233,10 @@ box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
 cursor:pointer;
 transform:scale(1.1);
 }
+#NextButton:active {
+  box-shadow: 0 7px 10px 0 rgba(0,0,0,0.24), 0 12px 30px 0 rgba(0,0,0,0.19);
+  transform:scale(1.05);
+}
 
 #PreviousButton {
 background-color: #f44336; /* Red */
@@ -254,29 +258,35 @@ box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
 cursor:pointer;
 transform:scale(1.1);
 }
+#PreviousButton:active {
+  box-shadow: 0 7px 10px 0 rgba(0,0,0,0.24), 0 12px 30px 0 rgba(0,0,0,0.19);
+  transform:scale(1.05);
+}
 
 .tablinks {
   background-color: inherit;
   float: left;
   border-width: 0.1em;
   border-color: darkgray;
-  border-bottom: none;
   border-top-left-radius: 1em;
   border-top-right-radius: 1em;
   outline: none;
   cursor: pointer;
   padding: 14px 16px;
   transition: 0.3s;
+  background-color: #ddd;
 }
 
 .tablinks:hover {
-  background-color: #ddd;
+
   transform:scale(1.1);
   box-shadow:1px 1px 3px rgba(0,0,0,0.5);
+  background-color: inherit;
 }
 
 .active {
-  background-color: #ccc;
+  background-color: inherit;
+  border-bottom: none;
 }
 
 
